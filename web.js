eda8204e209b1f90c57fb6a9abbb7d4540b1fb7b
jmks <jason.schweier@gmail.com>
2013-07-11 17:46:42 -0400

    added sync file reading via buffer

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // read index.html from disk and respond with it
+  var buffer = new Buffer(fs.readFileSync("index.html"), "utf-8");
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
